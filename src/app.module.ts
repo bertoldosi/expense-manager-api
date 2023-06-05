4e9aad157e8d6e6c36d4c0dccee681ca7fd5d7db
bertoldosi <matheus.bertoldosi@gmail.com>
2023-06-05 13:38:33 -0300

    feat: Implementando novos resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './resource/user/users.module';
 import { ExpensesModule } from './resource/expense/expenses.module';
+import { ShoppingModule } from './resource/shopping/shopping.module';
+import { InstitutionModule } from './resource/institution/institution.module';
 
 @Module({
-  imports: [UsersModule, ExpensesModule],
+  imports: [UsersModule, ExpensesModule, ShoppingModule, InstitutionModule],
   controllers: [AppController],
   providers: [AppService],
 })
